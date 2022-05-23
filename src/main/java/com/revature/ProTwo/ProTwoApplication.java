72a0ee620fd9be5314efdf435078935c248ecd63
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-05-23 16:58:57 -0500

    added frontend to repo

--- src/main/java/com/revature/ProTwo/ProTwoApplication.java
+++ src/main/java/com/revature/ProTwo/ProTwoApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.revature.ProTwo.Utilities.TMDBApi;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class ProTwoApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(ProTwoApplication.class, args);
+		SpringApplication.run(ProTwoApplication.class, args);
 		
-		System.out.println(TMDBApi.getById(799876));
+		//System.out.println(TMDBApi.getById(799876));
 	}
 
 
